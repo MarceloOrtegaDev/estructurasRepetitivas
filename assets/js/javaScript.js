659ce22d023508c706b20c0a2457bbe076550337
MarceloOrtegaDev <ortegamarcelodavid77@gmail.com>
2024-05-13 10:48:25 -0300

    Se crearon los archivos y se sube el repositorio de la actividad

---
+++ assets/js/javaScript.js
@@ -0,0 +1,12 @@
+let objeto = {
+    nombreCompleto:"Gaston",
+    edad: 23,					
+    rol:"jungla",
+    vivo:true
+    }
+
+    let persona = ["hola",1, 1n, "etc"]
+
+for (let i=0; i < persona.length; i++){
+    console.log(persona[i])
+}
